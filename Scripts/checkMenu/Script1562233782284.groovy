3d5d38f41afa89e1e7df22237afd354701355877
dhavalmahajan9016@gmail.com <dhavalmahajan9016@gmail.com>
2019-07-10 11:48:50 +0530

    all files added

---
+++ Scripts/checkMenu/Script1562233782284.groovy
@@ -0,0 +1,26 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import internal.GlobalVariable as GlobalVariable
+import groovy.json.JsonSlurper
+
+
+JsonSlurper slurper = new JsonSlurper()
+def result = slurper.parseText('{"person":{"name":"Guillaume","age":33,"pets":["dog","cat"]}}')
+
+assert result.person.name == "Guillaume"
+assert result.person.age  == 33
+assert result.person.pets[0]  == "dog"
+
+
